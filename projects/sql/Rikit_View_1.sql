bae853a2b57a109ae07f5512043e60fde6c564f2
rikit03 <rikitthapa2003@outlook.com>
2025-11-24 21:41:48 -0500

    Delete projects/sql directory

--- projects/sql/Rikit_View_1.sql
+++
@@ -1,9 +0,0 @@
---USE [AdventureWorksLT2019]
-GO
---Name: rikit thapa
---student_id : 500232522
-
-SELECT TOP (100) PERCENT ProductCategoryID, Name
-FROM     SalesLT.ProductCategory
-ORDER BY ProductCategoryID, Name
-
